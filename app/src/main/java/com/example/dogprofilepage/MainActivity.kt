000aa1afbba24d7e5c88354f8fd4bdb94de4ae9f
hamzak <Mrlawmaker53>
2022-06-20 11:15:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dogprofilepage/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.dogprofilepage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.dogprofilepage.ui.theme.DogProfilePageTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DogProfilePageTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                   // MyApp()
+                ProfilePage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DogProfilePageTheme {
+        MyApp()
+    }
+}
+
+@Composable
+fun MyApp()
+{
+    Text(text="Welcome to this video",
+    fontSize=30.sp)
+}
